873a69733da2678773797de15533d0bbd1c05c69
Mario Petrovski <marivce12@gmail.com>
2021-02-22 21:49:02 +0100

    BSH-6

---
+++ src/test/java/mk/iwec/bookshelf/test/integration/utils/AuthorTestUtil.java
@@ -0,0 +1,26 @@
+package mk.iwec.bookshelf.test.integration.utils;
+
+import mk.iwec.bookshelf.domain.Author;
+import mk.iwec.bookshelf.dto.AuthorDto;
+
+
+
+
+public class AuthorTestUtil {
+    public static Author createMockAuthorEntity() {
+        Author mock = new Author();
+        mock.setId(1);
+        mock.setFirstName("Mock FirstName");
+        mock.setLastName("Mock LastName");
+        mock.setSex(Author.Sex.MALE);
+        return mock;
+    }
+
+    public static AuthorDto createMockAuthorDto(String firstName, String lastName) {
+        AuthorDto mock = new AuthorDto();
+        mock.setFirstName(firstName);
+        mock.setLastName(lastName);
+        return mock;
+    }
+
+}
