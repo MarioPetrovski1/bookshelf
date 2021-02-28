a6a6a77b83e245f11a241ff015df1a004e6e3e9f
Mario Petrovski <marivce12@gmail.com>
2021-02-28 13:57:48 +0100

    Added @CrossOrigin (CORS)

--- backend/src/main/java/mk/iwec/bookshelf/api/PublisherController.java
+++ backend/src/main/java/mk/iwec/bookshelf/api/PublisherController.java
@@ -11,20 +11,13 @@ import mk.iwec.bookshelf.dto.PublisherInfoDto;
 import mk.iwec.bookshelf.mapper.PublisherMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import mk.iwec.bookshelf.domain.Publisher;
 import mk.iwec.bookshelf.infrastucture.Endpoints;
 import mk.iwec.bookshelf.service.impl.PublisherServiceImpl;
 
+@CrossOrigin
 @RestController
 @RequestMapping(Endpoints.PUBLISHERS)
 public class PublisherController {
